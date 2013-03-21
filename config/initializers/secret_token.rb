cf7b1c991d92e1e61d70397cf47ed3fba30ed9f8
naveen <moranaveen1@gmail.com>
2013-03-21 12:34:50 +0530

    Added database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleDemo::Application.config.secret_token = 'f0aae2f0c4cd47749fae4c794a66bd5504a563af65dcb5c5b3d3387ff0dba4f7b733cb8152a4bf821b195b26b2a46667920c0f6d6ec4564bcd8f1a3416be7112'
